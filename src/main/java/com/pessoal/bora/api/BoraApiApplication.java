c73dfde238b93a5201e63a27304460abc3458f8e
Sandro M. dos Santos <sandromarista@gmail.com>
2025-08-22 08:32:44 -0300

    Project created

---
+++ src/main/java/com/pessoal/bora/api/BoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.pessoal.bora.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pessoal/bora/api/BoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoal.bora.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
