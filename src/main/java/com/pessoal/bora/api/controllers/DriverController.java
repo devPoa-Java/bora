c9b7fca3fed5d463a47b92a5e36cc70b24201d4f
Sandro M. dos Santos <sandromarista@gmail.com>
2025-08-22 11:49:52 -0300

    Method findById

--- src/main/java/com/pessoal/bora/api/controllers/DriverController.java
+++ src/main/java/com/pessoal/bora/api/controllers/DriverController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class DriverController {
 	public List<DriverDTO> findAll(){
 		List<DriverDTO> listDrivers = driverService.findAll();
 		return listDrivers;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public DriverDTO findById(@PathVariable Long id) {
+		return driverService.findById(id);
 	}
 
 }
--- src/main/java/com/pessoal/bora/api/services/DriverService.java
+++ src/main/java/com/pessoal/bora/api/services/DriverService.java
@@ -3,7 +3,10 @@ package com.pessoal.bora.api.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.pessoal.bora.api.domain.Driver;
 import com.pessoal.bora.api.dto.DriverDTO;
@@ -15,9 +18,15 @@ public class DriverService {
 	@Autowired
 	private DriverRepository driverRepository;
 	
-	
+	@Transactional(readOnly = true)
 	public List<DriverDTO> findAll(){
 		List<Driver> list = driverRepository.findAll();
 		return list.stream().map(x -> new DriverDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public DriverDTO findById(Long id) {
+		Driver driver =  driverRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	    return  new DriverDTO(driver);
 	}
 }
